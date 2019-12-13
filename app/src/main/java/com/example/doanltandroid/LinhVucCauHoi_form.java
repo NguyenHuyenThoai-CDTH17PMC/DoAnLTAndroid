d1ca86843ff2618c7c4863eabd444799b4deef64
TranTheVinh-CDTH17PMC <56820186+TranTheVinh-CDTH17PMC@users.noreply.github.com>
2019-12-13 17:20:38 +0700

    vvvvv

--- app/src/main/java/com/example/doanltandroid/LinhVucCauHoi_form.java
+++ app/src/main/java/com/example/doanltandroid/LinhVucCauHoi_form.java
@@ -35,18 +35,7 @@ public class LinhVucCauHoi_form extends AppCompatActivity {
     private RecyclerView recyclerView;
     private LinhVucAdapter linhVucAdapter;
     private ArrayList<LinhVuc>linhVucs;
-<<<<<<< HEAD
-    private String đuongdan="http://192.168.56.1:8080/Do_An_PHP/public/api/linh-vuc";
-=======
-<<<<<<< HEAD
-    private String đuongdan="http://192.168.56.1/Do_An_PHP/public/api/linh-vuc";
-=======
-    private String đuongdan="http://192.168.1.17:8080/Do_An_PHP/public/api/linh-vuc";
     private String id_nguoichoi;
->>>>>>> f5e41092b76e7abf278e7dd8b7a93a267ea5865d
-
-
->>>>>>> 00519d1d2e366700e4c3a13fe538add506b60ad4
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
